093f2f607dc1cadefefe7bce6fdfcb6a6898ec18
James <jamesncox@yahoo.com>
2019-04-26 11:20:31 -0500

    menu and goodbye methods

--- lib/wine/cli.rb
+++ lib/wine/cli.rb
@@ -5,6 +5,7 @@ class TopWines::class
     def call
         list_wines
         menu
+        goodbye
     end
 
     def list_wines
@@ -18,7 +19,22 @@ class TopWines::class
     end
 
     def menu
-        puts "Enter the number of the wine you'd like more info on:"
+        puts "Enter the number of the wine you'd like more info on or type exit to enter:"
+        while input != "exit"
+            input = gets.strip.downcase
+            case input
+                when "1"
+                    puts "More info on wine 1..."
+                when "2"
+                    puts "More info on wine 2..."
+                when "3"
+                    puts "More info on wine 3..."
+            end
+        end
+    end
+
+    def goodbye
+        puts "See you next time for more great wines under $20!"
     end
 
 end
