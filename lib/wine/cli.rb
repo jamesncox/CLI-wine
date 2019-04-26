aa5429c8a948ef9128552ba9d1cedc4aab811380
James <jamesncox@yahoo.com>
2019-04-26 10:38:56 -0500

    Hard-coding list_wines

--- lib/wine/cli.rb
+++ lib/wine/cli.rb
@@ -8,9 +8,12 @@ class TopWines::class
     end
 
     def list_wines
-        1. Domaines Schlumberger Les Princes Abbes Riesling 2014 - 95 points - $19.99
-        2. Fabre Montmayou Reserva Cabernet Franc 2016 - 96 points - $15.99
-        3. Susana Balbo Signature Malbec 2016 - 95 points - $19.99 
+        puts <<-DOC.gsub /^\s*/, ''
+            1. Domaines Schlumberger Les Princes Abbes Riesling 2014 - 95 points - $19.99
+            2. Fabre Montmayou Reserva Cabernet Franc 2016 - 96 points - $15.99
+            3. Susana Balbo Signature Malbec 2016 - 95 points - $19.99 
+        DOC
+        end
     end
 
 end
