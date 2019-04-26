8acf381e729aceb9b55828cecfda516a3b44772c
James <jamesncox@yahoo.com>
2019-04-26 11:10:08 -0500

    Added menu method

--- lib/wine/cli.rb
+++ lib/wine/cli.rb
@@ -3,17 +3,22 @@
 class TopWines::class
 
     def call
-        puts "The BEST wines under $20!"
         list_wines
+        menu
     end
 
     def list_wines
+        puts "The BEST wines under $20!"
         puts <<-DOC.gsub /^\s*/, ''
             1. Domaines Schlumberger Les Princes Abbes Riesling 2014 - 95 points - $19.99
             2. Fabre Montmayou Reserva Cabernet Franc 2016 - 96 points - $15.99
             3. Susana Balbo Signature Malbec 2016 - 95 points - $19.99 
         DOC
         end
+    end
+
+    def menu
+        puts "Enter the number of the wine you'd like more info on:"
     end
 
 end
