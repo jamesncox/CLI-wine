c9be146b440568ca349a072d96aa410d7b93865f
James <jamesncox@yahoo.com>
2019-04-26 10:33:18 -0500

    Added fake method

--- lib/wine/cli.rb
+++ lib/wine/cli.rb
@@ -4,7 +4,13 @@ class TopWines::class
 
     def call
         puts "The BEST wines under $20!"
-        top_wines
+        list_wines
+    end
+
+    def list_wines
+        1. Domaines Schlumberger Les Princes Abbes Riesling 2014 - 95 points - $19.99
+        2. Fabre Montmayou Reserva Cabernet Franc 2016 - 96 points - $15.99
+        3. Susana Balbo Signature Malbec 2016 - 95 points - $19.99 
     end
 
 end
