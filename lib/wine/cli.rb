928de463a3c88760836d49861f445926cfd64049
James <jamesncox@yahoo.com>
2019-04-26 14:50:44 -0500

    CLI changes

--- lib/wine/cli.rb
+++ lib/wine/cli.rb
@@ -9,29 +9,28 @@ class TopWines::CLI
     end
 
     def list_wines
-        puts "The BEST wines under $20!"
-        puts <<-DOC.gsub /^\s*/, ''
-            1. Domaines Schlumberger Les Princes Abbes Riesling 2014 - 95 points - $19.99
-            2. Fabre Montmayou Reserva Cabernet Franc 2016 - 96 points - $15.99
-            3. Susana Balbo Signature Malbec 2016 - 95 points - $19.99
-        DOC
+        Scraper.get_names("https://www.wine.com/list/wine/7155?pricemax=20&ratingmin=95")
+        # puts "The BEST wines under $20!"
+        # puts <<-DOC.gsub /^\s*/, ''
+        #     1. Domaines Schlumberger Les Princes Abbes Riesling 2014 - 95 points - $19.99
+        #     2. Fabre Montmayou Reserva Cabernet Franc 2016 - 96 points - $15.99
+        #     3. Susana Balbo Signature Malbec 2016 - 95 points - $19.99
+        # DOC
 
     end
 
     def menu
-        puts "Enter the number of the wine you'd like more info on or type exit to enter:"
-        input = gets.strip.downcase
+        input = nil
         while input != "exit"
+            puts "Enter the number of the wine you'd like more info on or type exit to enter:"
+            input = gets.strip.downcase 
             case input
                 when "1"
                     puts "More info on wine 1..."
-                    break
                 when "2"
                     puts "More info on wine 2..."
-                    break
                 when "3"
                     puts "More info on wine 3..."
-                    break
             end
         end
     end
