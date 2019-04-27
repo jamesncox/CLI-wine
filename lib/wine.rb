3a47c99cbb9150b24f26b4135e4b0d1797224456
James <jamesncox@yahoo.com>
2019-04-27 08:18:30 -0500

    refractor scraper

--- lib/wine.rb
+++ lib/wine.rb
@@ -2,6 +2,7 @@
 
 require 'open-uri'
 require 'nokogiri'
+# require 'pry'
 
 require_relative 'wine/version'
 require_relative 'wine/cli'
--- lib/wine/wine_scraper.rb
+++ lib/wine/wine_scraper.rb
@@ -1,4 +1,5 @@
 # require 'nokogiri'
+require 'pry'
 
 class Scraper
     
@@ -7,15 +8,36 @@ class Scraper
         
     # end
 
-    def self.get_names(url)  
-        data = Nokogiri::HTML(open(url)).css("ul.prodList").css("li.prodItem")
+    #new class called self.get_wine(url)
+    #scrape for prodItem
+    #then separate attributes inside self.get_wine(url)
+
+    def self.get_winesI(url)
+        binding.pry
+        main_scrape = Nokogiri::HTML(open(url)).css()
+    end
+
+    # def self.get_names(url)  
+    #     data = Nokogiri::HTML(open(url)).css("ul.prodList").css("li.prodItem")
         
-        names = data.map do |item|
-            item.css("div.prodItemInfo").css("span.prodItemInfo_name").text
-         end 
-        # return names do NOT output
-         names.each.with_index(1) {|item, index| puts "#{index}: #{item}"}
-    end 
+    #     names = data.map do |item|
+    #         item.css("div.prodItemInfo").css("span.prodItemInfo_name").text
+
+    #     end 
+    #     # return names do NOT output
+    #     names.each.with_index(1) {|item, index| puts "#{index}: #{item}"}
+    # end 
+
+    # {k:, [v:, v2:, v3:]}
+
+    # wine_names = []
+    # fill wine_names array with key/value pairs like in student scraper
+
+    # [{name: "good_malbec", price: "$15", rating: "90", url: "goodwine.com"}, {name: "okay_syrah", price: "17",}]
+    
+    # { good_malbec: {price: 100, rating: "good", url: "link"},
+    #   syrah: {price: 70, rating: "shit", url: "link" }
+    # }   
 
     # master = []
 
@@ -24,7 +46,21 @@ class Scraper
     # names = []
     # prices = []
     # ratings = []
-    # urls = []
+    # urls = [] ??????
+
+# [5] pry(Scraper)> x.map {|whatever| {name: whatever.css("span.prodItemInfo_name").text}}
+# => [{:name=>"Fabre Montmayou Reserva Cabernet Franc 2016"},
+#  {:name=>"Susana Balbo Signature Malbec 2016"},
+#  {:name=>"Domaines Schlumberger Les Princes Abbes Riesling 2014"},
+#  {:name=>"Losada Bierzo 2015"},
+#  {:name=>"Graham's Six Grapes Reserve Port"},
+#  {:name=>"Carpano Antica Formula Sweet Vermouth (375ML)"},
+#  {:name=>"Robert Oatley Signature Cabernet Sauvignon 2017"},
+#  {:name=>"Graham's Six Grapes Reserve Port (375ML half-bottle)"},
+#  {:name=>"Chateau Doisy Vedrines Sauternes (375ML Futures Pre-Sale) 2016"},
+#  {:name=>"Chateau Doisy Vedrines Sauternes (375ML Futures Pre-Sale) 2018"}]
+# [6] pry(Scraper)>
+
 end
 
 
