58fbadb9ffa397005af98445c82bb4245a9a18b7
James <jamesncox@yahoo.com>
2019-05-07 17:03:17 -0500

    clean up/refactor

--- lib/top_wines/top_wines.rb
+++ lib/top_wines/top_wines.rb
@@ -1,9 +1,3 @@
-#create wine objects
-
-# 1. Domaines Schlumberger Les Princes Abbes Riesling 2014, 95 points, $19.99
-# 2. Fabre Montmayou Reserva Cabernet Franc 2016, 96 points, $15.99
-# 3. Susana Balbo Signature Malbec 2016, 95 points, $19.99
-
 class TopWines::Wine
     attr_accessor :name, :rating, :price, :description
 
